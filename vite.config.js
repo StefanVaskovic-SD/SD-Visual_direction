8e1aff946f5f45394e8b7ba071b0e7b817a31f4f
StefanVaskovic-SD <stefan.vaskovic@studiodirection.com>
2025-12-10 11:46:37 +0100

    Configure Vite for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true,
+  },
+  server: {
+    port: process.env.PORT || 5173,
+    host: true,
+  },
 })
 
